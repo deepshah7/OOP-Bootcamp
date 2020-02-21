19440cdeaba2cf299b2ffecc94d71da581e25e2d
Deep Shah <deep@makkajai.com>
2020-02-21 11:09:37 +0530

    Bloppians - Task 1

--- Bloppians/src/FemaleBloppian.java
+++ Bloppians/src/FemaleBloppian.java
@@ -1,2 +1,8 @@
-public class FemaleBloppian {
+public class FemaleBloppian extends Bloppian {
+    public String layEgg() { return "hmmm, which came first, the bloppian or the egg?"; }
+
+    @Override
+    public String fly() {
+        return "I just overtook concorde!";
+    }
 }
--- Bloppians/src/MaleBloppian.java
+++ Bloppians/src/MaleBloppian.java
@@ -1,2 +1,10 @@
-public class MaleBloppian {
+public class MaleBloppian extends Bloppian {
+    public String fertiliseEgg() {
+        return "This egg is not for eating!!";
+    }
+
+    @Override
+    public String talk() {
+        return "I AM SHOUTING!!";
+    }
 }
