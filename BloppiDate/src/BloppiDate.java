4a3b9cff3569611800ef36befec081a544f16ec1
Deep Shah <deep@makkajai.com>
2020-02-21 10:48:41 +0530

    Exercise 3 Done

--- BloppiDate/src/BloppiDate.java
+++ BloppiDate/src/BloppiDate.java
@@ -1,27 +1,33 @@
 
 public class BloppiDate {
-    private final int year;
-    private final int month;
-    private final int day;
+
+    private final int daysSinceEpoch;
 
     public BloppiDate(int year, int month, int day) {
         if (month > 11 || month < 0) { throw new IllegalArgumentException("Month out of bounds"); }
         if (day > 29 || day < 0) { throw new IllegalArgumentException("Day out of bounds"); }
 
-        this.year = year;
-        this.month = month;
-        this.day = day;
+        daysSinceEpoch = TotalDays(year, month, day);
+    }
+
+    public BloppiDate(int days) {
+        daysSinceEpoch = days;
+    }
+
+    private static int TotalDays(int year, int month, int day) {
+        int multiplier = year >= 0? 1 : -1;
+        return (Math.abs(year)*360 + (month) * 30 + (day)) * multiplier;
     }
 
     public String toString() {
-        return day + "/" + month + "/" + year;
+        return Math.abs(daysSinceEpoch % 30) + "/" + Math.abs((daysSinceEpoch / 30) % 12) + "/" + (daysSinceEpoch / 30 / 12);
     }
 
     public BloppiDate AddDays(int days) {
-        return new BloppiDate(year + (days / 30) / 12, month + (days / 30) % 12, day + days % 30);
+        return new BloppiDate(this.daysSinceEpoch + days);
     }
 
     public int DiffDays(BloppiDate other) {
-        return (other.year - this.year)*360 + (other.month - this.month) * 30 + (other.day - this.day);
+        return (other.daysSinceEpoch - this.daysSinceEpoch);
     }
 }
